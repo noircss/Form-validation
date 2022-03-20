f90d95390815fca6873ee97fb52a675cfc06bda5
ShahRukh Ahmed <badshah21204@gmail.com>
2022-03-20 13:04:40 -0400

    Updates

---
+++ precache-manifest.5d62485f483b67c36c2f47ef06820263.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "85ad66131a86486be11d",
+    "url": "/form-validation/static/css/main.57471fc3.chunk.css"
+  },
+  {
+    "revision": "85ad66131a86486be11d",
+    "url": "/form-validation/static/js/main.bf7dbd99.chunk.js"
+  },
+  {
+    "revision": "7398d6737311475e4b32",
+    "url": "/form-validation/static/js/runtime~main.5042e885.js"
+  },
+  {
+    "revision": "a5e9d36056a6ac6e590a",
+    "url": "/form-validation/static/js/2.f4f735b6.chunk.js"
+  },
+  {
+    "revision": "a80594e835613a5e010e0511c67c831a",
+    "url": "/form-validation/index.html"
+  }
+];
